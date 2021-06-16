01e51aef225e2a6798afa0e564290ea2640ae5a6
harrisonblackburn <blackbhr@yahoo.com>
2021-06-16 15:08:37 -0500

    working on frontend to display cards

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -56,39 +56,24 @@ function SearchBooks() {
                     </Button>
 
       </form>
-      <div className="container row justify-content-center">
       {result.map(book => (
-                    <div className="card col-3 mx-2 my-2" style={{ "width": "18rem" }}>
-                        
-                        <div className="card-body">
-                            <h4 className="card-title card-header mb-3 text-center">
-                                {book.volumeInfo.title}
-                            </h4>
-                            <h6 className="card-subtitle">  {book.volumeInfo.authors[0]}</h6>
-                            
-                            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
-                            <p className="card-text">{book.volumeInfo.description}</p>
-                            <a href="#" className="btn btn-secondary btn-sm">{book.volumeInfo.infoLink} </a>
+                     <div className="card col-lg-3 mx-2 my-2 border border-dark">
+                        <h2 className="card-header fluid">{book.volumeInfo.title}</h2>
+                        <h4 className="card-subtitle mb-2"> {book.volumeInfo.authors[0]}</h4>
+                        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} className="card-img"/>
+                        <p className="card-text">{book.volumeInfo.description}</p>
+                        <a href="#" className="btn btn-secondary btn-sm"> </a>
+                           
                         </div>
-                    </div>
 
           
       ))}
     </div>
-  </div>
   </>
+  
 
   )
   };
 
 
   export default SearchBooks;
-
-   {/* {result.map(book => (
-        
-
-        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
-          {book.volumeInfo.title}
-          {book.volumeInfo.authors[0]}
-          {book.volumeInfo.description}
-          {book.volumeInfo.infoLink} */}
